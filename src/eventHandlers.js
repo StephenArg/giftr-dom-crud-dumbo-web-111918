9aa7642ce76be043ec34e2493549a411315c394b
Stephen Argentina <churchman1993@yahoo.com>
2019-01-10 16:34:21 -0500

    Changes

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,1 +1,24 @@
 // You can add your event handlers here! 
+
+let giftList = document.getElementsByClassName("gift-list")
+
+
+document.addEventListener("DOMContentLoaded", function(){
+if (gifts.length != 0) {
+        for (element of gifts) {
+            let li = document.createElement("li")
+            let img = document.createElement("img")
+            img.src = element.image
+            img.className = "gift-images"
+            let label = document.createElement('label')
+            label.innerText = element.name
+            img.appendChild(label)
+            li.appendChild(img)
+            giftList[0].appendChild(li)
+        }
+} else {
+    let li = document.createElement("li")
+    li.innerText = "No gifts yet :("
+    giftList[0].appendChild(li)
+}
+})
